09d7cb467cc054d3aa882b42d01c8dc623d4689b
admin <andrei.tupitsin@gmail.com>
2024-11-05 23:57:25 +0200

    Первый коммит

---
+++ MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Learn_a_language
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("JosefinSans-SemiBold.ttf", "OpenSansRegular");
+                    fonts.AddFont("JosefinSans-SemiBold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
